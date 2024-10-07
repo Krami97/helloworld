907b5d735ab161f88cdf91507620ee79a4900b4e
Krami97 <tin.kramaric@gmail.com>
2024-10-07 19:06:10 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
         System.out.println("Hello Tin");
         System.out.println("treci sat predavanja");
+        System.out.println("cetvrti sat predavanja");
+
 
 
     }
